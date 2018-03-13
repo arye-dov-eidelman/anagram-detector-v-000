f040253b4989ab3f3e83eeac390dbb4a773d1eec
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-13 22:37:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,11 @@
 class Anagram
   def initialize(subject)
-    @subject = subject.split('').sort
+    @subject = subject
   end
 
   def match(words)
     words.select do |word|
-      word.split('').sort == @subject
+      word.split('').sort == @subject.split('').sort
     end
   end
 end
